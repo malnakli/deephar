ff6091c7c2788144af2b076743fc7db188785411
m_aln <m_aln@encs.concordia.ca>
2019-12-04 20:42:47 -0500

    finish PredictionBlockPS

--- pytorch/helper.py
+++ pytorch/helper.py
@@ -35,7 +35,6 @@ class Conv2dBatchActivate(nn.Module):
 
 
 class Conv2dBatch(nn.Module):
-
     def __init__(self, in_channels, out_channels, kernel_size, stride, padding=0):
         super().__init__()
         self.conv = nn.Conv2d(
@@ -95,13 +94,38 @@ class SeparableConv2d(nn.Module):
     https://github.com/Cadene/pretrained-models.pytorch/blob/master/pretrainedmodels/models/xception.py#L50
     """
 
-    def __init__(self, in_channels, out_channels, kernel_size=1, stride=1, padding=0, dilation=1, bias=False):
+    def __init__(
+        self,
+        in_channels,
+        out_channels,
+        kernel_size=1,
+        stride=1,
+        padding=0,
+        dilation=1,
+        bias=False,
+    ):
         super().__init__()
 
-        self.conv1 = nn.Conv2d(in_channels, in_channels, kernel_size,
-                               stride, padding, dilation, groups=in_channels, bias=bias)
+        self.conv1 = nn.Conv2d(
+            in_channels,
+            in_channels,
+            kernel_size,
+            stride,
+            padding,
+            dilation,
+            groups=in_channels,
+            bias=bias,
+        )
         self.pointwise = nn.Conv2d(
-            in_channels, out_channels, kernel_size=1, stride=1, padding=0, dilation=1, groups=1, bias=bias)
+            in_channels,
+            out_channels,
+            kernel_size=1,
+            stride=1,
+            padding=0,
+            dilation=1,
+            groups=1,
+            bias=bias,
+        )
 
         self.weights = [self.conv1.weight, self.pointwise.weight]
 
@@ -112,13 +136,15 @@ class SeparableConv2d(nn.Module):
 
 
 class ActivateSeparableConv2dBatch(nn.Module):
-
-    def __init__(self, in_channels, out_channels, kernel_size=1, stride=1, padding=0, bias=False):
+    def __init__(
+        self, in_channels, out_channels, kernel_size=1, stride=1, padding=0, bias=False
+    ):
         super().__init__()
 
         self.relu = nn.ReLU(inplace=True)
         self.conv = SeparableConv2d(
-            in_channels, out_channels, kernel_size, stride, padding, bias=bias)
+            in_channels, out_channels, kernel_size, stride, padding, bias=bias
+        )
         self.bn = nn.BatchNorm2d(
             out_channels,
             eps=0.001,  # value found in tensorflow
@@ -134,7 +160,6 @@ class ActivateSeparableConv2dBatch(nn.Module):
 
 
 class SeparableResidualModule(nn.Module):
-
     def __init__(self, in_channels, out_channels, kernel_size=3, padding=0):
         super().__init__()
 
@@ -142,10 +167,12 @@ class SeparableResidualModule(nn.Module):
         self.out_channels = out_channels
 
         self.conv1 = ActivateConv2dBatch(
-            in_channels, out_channels, kernel_size=1, stride=1)
+            in_channels, out_channels, kernel_size=1, stride=1
+        )
 
         self.conv2 = ActivateSeparableConv2dBatch(
-            in_channels, out_channels, kernel_size=kernel_size, padding=padding)
+            in_channels, out_channels, kernel_size=kernel_size, padding=padding
+        )
 
     def forward(self, x):
         if self.in_channels == self.out_channels:
@@ -158,7 +185,6 @@ class SeparableResidualModule(nn.Module):
 
 
 class Lambda(nn.Module):
-
     def __init__(self, lambd):
         super().__init__()
         self.lambd = lambd
@@ -168,15 +194,12 @@ class Lambda(nn.Module):
 
 
 class SoftArgMax2d(nn.Module):
-
     def __init__(self, in_channels, out_channels, kernel_size=1, stride=1, padding=0):
         super().__init__()
 
-        self.num_rows = kernel_size if isinstance(
-            kernel_size, int) else kernel_size[0]
+        self.num_rows = kernel_size if isinstance(kernel_size, int) else kernel_size[0]
 
-        self.num_cols = kernel_size if isinstance(
-            kernel_size, int) else kernel_size[1]
+        self.num_cols = kernel_size if isinstance(kernel_size, int) else kernel_size[1]
 
         self.num_filters = out_channels
         self.in_channels = in_channels
@@ -194,8 +217,9 @@ class SoftArgMax2d(nn.Module):
 
     def _softmax(self, x):
         if x.ndim != 4:
-            raise ValueError('This function is specific for 4D tensors. '
-                             'Here, ndim=' + str(x.ndim))
+            raise ValueError(
+                "This function is specific for 4D tensors. " "Here, ndim=" + str(x.ndim)
+            )
 
         e = torch.exp(x - torch.max(x, dim=(1, 2), keepdim=True))
         s = torch.sum(e, dim=(1, 2), keepdim=True)
@@ -204,7 +228,13 @@ class SoftArgMax2d(nn.Module):
     def _lin_interpolation_2d(self, x, dim):
 
         conv = SeparableConv2d(
-            self.in_channels, self.num_filters, (self.num_rows, self.num_cols), self.stride, self.padding, bias=False)
+            self.in_channels,
+            self.num_filters,
+            (self.num_rows, self.num_cols),
+            self.stride,
+            self.padding,
+            bias=False,
+        )
         x = conv(x)
 
         ws = conv.weights
@@ -215,7 +245,7 @@ class SoftArgMax2d(nn.Module):
 
         for i in range(self.num_filters):
             ws[0].data[:, :, i, 0] = linspace[:, :]
-            ws[1].data[0, 0, i, i] = 1.
+            ws[1].data[0, 0, i, i] = 1.0
 
         for p in self.conv1.parameters():
             p.requires_grad = False
@@ -227,7 +257,6 @@ class SoftArgMax2d(nn.Module):
         return x
 
     def _linspace_2d(self, nb_rols, nb_cols, dim=0):
-
         def _lin_sp_aux(size, nb_repeat, start, end):
             linsp = torch.linspace(start=start, end=end, num=size)
             x = torch.empty((nb_repeat, size), dtype=torch.float32)
@@ -242,6 +271,192 @@ class SoftArgMax2d(nn.Module):
         return _lin_sp_aux(nb_cols, nb_rols, 0.0, 1.0)
 
 
+class BuildJointsProbability(nn.Module):
+    def __init__(self, kernel_size=1, stride=1, padding=0):
+        super().__init__()
+
+        self.max = nn.MaxPool2d(kernel_size=kernel_size, stride=stride, padding=padding)
+        self.act = nn.Sigmoid()
+
+    def forward(self, x):
+        x = self.max(x)
+        x = self.act(x)
+
+        x = Lambda(lambda x: torch.squeeze(x, dim=1))(x)
+        x = Lambda(lambda x: torch.squeeze(x, dim=1))(x)
+        # dim 0 becasue the channel is first in pytorch.
+        x = Lambda(lambda x: torch.unsqueeze(x, axis=0))(x)
+        return x
+
+
+class BuildContextAggregation(nn.Module):
+    def __init__(self, in_features, alpha, num_joints=16, num_context=1, num_frames=1):
+        super().__init__()
+
+        self.num_joints = num_joints
+        self.num_frames = num_frames
+        self.in_features = in_features
+        self.alpha = alpha
+        self.num_context = num_context
+
+    def forward(self, ys, yc, pc):
+
+        if self.num_frames == 1:
+            # Define inputs
+            # ys = torch.Tensor(2, self.num_joints)
+            # yc = torch.Tensor(2, self.num_joints * self.num_context)
+            # pc = torch.Tensor(1, self.num_joints * self.num_context)
+
+            # Split contextual predictions in x and y and do computations separately
+            xi = Lambda(lambda x: x[:, :, 0:1])(yc)
+            yi = Lambda(lambda x: x[:, :, 1:2])(yc)
+        else:
+            # ys = Input(shape=(num_frames, num_joints, 2))
+            # yc = Input(shape=(num_frames, num_joints * num_context, 2))
+            # pc = Input(shape=(num_frames, num_joints * num_context, 1))
+
+            # Split contextual predictions in x and y and do computations separately
+            xi = Lambda(lambda x: x[:, :, :, 0:1])(yc)
+            yi = Lambda(lambda x: x[:, :, :, 1:2])(yc)
+
+        # Define auxiliary layers.
+        mul_alpha = Lambda(lambda x: self.alpha * x)
+        mul_1alpha = Lambda(lambda x: (1 - self.alpha) * x)
+
+        tf_div = Lambda(lambda x: torch.divide(x[0], x[1]))
+
+        pxi = torch.mul(xi, pc)
+        pyi = torch.mul(yi, pc)
+
+        pc_sum = self._ctx_sum(pc, self.num_frames)
+        pxi_sum = self._ctx_sum(pxi, self.num_frames)
+        pyi_sum = self._ctx_sum(pyi, self.num_frames)
+
+        pxi_div = tf_div([pxi_sum, pc_sum])
+        pyi_div = tf_div([pyi_sum, pc_sum])
+        yc_div = torch.cat([pxi_div, pyi_div])
+
+        ys_alpha = mul_alpha(ys)
+        yc_div_1alpha = mul_1alpha(yc_div)
+
+        y = torch.add([ys_alpha, yc_div_1alpha])
+
+        return y
+
+    def _ctx_sum(self, inp, num_frames=1):
+        class _Ctx(nn.Module):
+            def __init__(self, in_features, num_joints=16, num_context=1):
+                super().__init__()
+
+                self.num_joints = num_joints
+                self.num_context = num_context
+
+                self.fc = nn.Linear(in_features, self.num_joints, bias=False)
+                self.act = nn.Sigmoid()
+
+            def forward(self, x):
+                # dim 0 becasue the channel is first in pytorch.
+                x = Lambda(lambda x: torch.squeeze(x, dim=0))(x)
+                x = self.fc(x)
+                x = Lambda(lambda x: torch.unsqueeze(x, dim=0))(x)
+
+                _w = self.fc.weight
+                _w.data = torch.zeros(_w.shape)  # w[0].fill(0)
+
+                for j in range(self.num_joints):
+                    _w.data[0][
+                        j * self.num_context : (j + 1) * self.num_context, j
+                    ] = 1.0
+
+                # d.trainable = False
+                for _p in self.fc.parameters():
+                    _p.requires_grad = False
+
+                return x
+
+        if num_frames > 1:
+            raise NotImplementedError
+
+        return _Ctx(
+            in_features=self.in_features,
+            num_joints=self.num_joints,
+            num_context=self.num_context,
+        )(inp)
+
+
+class BuildReceptionBlock(nn.Module):
+    def __init__(self, kernel_size):
+        super().__init__()
+
+        self.layer0_b = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+        self.layer1_b = ActivateConv2dBatch(
+            in_channels=576, out_channels=288, kernel_size=1
+        )
+        self.layer2_b = SeparableResidualModule(
+            in_channels=288, out_channels=288, kernel_size=kernel_size, padding=2
+        )
+
+        self.layer3_c = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+        self.layer4_c = SeparableResidualModule(
+            in_channels=288, out_channels=288, kernel_size=kernel_size, padding=2
+        )
+
+        self.layer5_a = SeparableResidualModule(
+            in_channels=576, out_channels=576, kernel_size=kernel_size, padding=2
+        )
+        self.layer5_b = SeparableResidualModule(
+            in_channels=288, out_channels=288, kernel_size=kernel_size, padding=2
+        )
+        self.layer5_c = SeparableResidualModule(
+            in_channels=288, out_channels=288, kernel_size=kernel_size, padding=2
+        )
+
+        self.layer6_c = SeparableResidualModule(
+            in_channels=288, out_channels=288, kernel_size=kernel_size, padding=2
+        )
+        self.layer7_c = nn.Upsample(scale_factor=2)
+        self.layer8_b = SeparableResidualModule(
+            in_channels=288, out_channels=576, kernel_size=kernel_size, padding=2
+        )
+        self.layer9_b = nn.Upsample(scale_factor=2)
+
+    def forward(self, x):
+        _b = self.layer0_b(x)
+        _b = self.layer1_b(_b)
+        _b = self.layer2_b(_b)
+        _c = self.layer3_c(_b)
+        _c = self.layer4_c(_c)
+        _a = self.layer5_a(x)
+        _b = self.layer5_b(_b)
+        _c = self.layer5_c(_c)
+        _c = self.layer6_c(_c)
+        _c = self.layer7_c(_c)
+        _b = torch.add(_b, _c)
+        _b = self.layer8_b(_b)
+        _b = self.layer9_b(_b)
+        x = torch.add(_a, _b)
+        return x
+
+
+def pose_regression_2d_context(
+    h, num_joints, sam_s_model, sam_c_model, jprob_c_model, agg_model, jprob_s_model
+):
+
+    # Split heatmaps for specialized and contextual information
+    hs = Lambda(lambda x: x[:, :, :, :num_joints])(h)
+    hc = Lambda(lambda x: x[:, :, :, num_joints:])(h)
+
+    # Soft-argmax and joint probability for each heatmap
+    ps = sam_s_model(hs)
+    pc = sam_c_model(hc)
+    vc = jprob_c_model(hc)
+
+    pose = agg_model(ps, pc, vc)
+    visible = jprob_s_model(hs)
+
+    return pose, visible, hs
+
+
 def conv2d_out_shape(width, height, Conv2d):
     """
     return (C , W , H)
@@ -252,18 +467,30 @@ def conv2d_out_shape(width, height, Conv2d):
     # taken from:
     # https://pytorch.org/docs/stable/nn.html#torch.nn.Conv2d
 
-    _h = math.floor(((
-        height
-        + (2 * Conv2d.padding[0])
-        - Conv2d.dilation[0] * (Conv2d.kernel_size[0] - 1)
-        - 1
-    ) / Conv2d.stride[0]) + 1)
+    _h = math.floor(
+        (
+            (
+                height
+                + (2 * Conv2d.padding[0])
+                - Conv2d.dilation[0] * (Conv2d.kernel_size[0] - 1)
+                - 1
+            )
+            / Conv2d.stride[0]
+        )
+        + 1
+    )
 
-    _w = math.floor(((
-        width
-        + (2 * Conv2d.padding[1])
-        - Conv2d.dilation[1] * (Conv2d.kernel_size[1] - 1)
-        - 1
-    ) / Conv2d.stride[1]) + 1)
+    _w = math.floor(
+        (
+            (
+                width
+                + (2 * Conv2d.padding[1])
+                - Conv2d.dilation[1] * (Conv2d.kernel_size[1] - 1)
+                - 1
+            )
+            / Conv2d.stride[1]
+        )
+        + 1
+    )
 
     return (Conv2d.out_channels, _w, _h)
--- pytorch/models.py
+++ pytorch/models.py
@@ -1,8 +1,17 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 from .helper import (
-    conv2d_out_shape, Conv2dBatchActivate, Conv2dBatch, ActivateConv2dBatch, SeparableResidualModule, ActivateSeparableConv2dBatch, SoftArgMax2d)
+    Conv2dBatchActivate,
+    Conv2dBatch,
+    ActivateConv2dBatch,
+    SeparableResidualModule,
+    ActivateSeparableConv2dBatch,
+    SoftArgMax2d,
+    BuildJointsProbability,
+    BuildContextAggregation,
+    BuildReceptionBlock,
+    pose_regression_2d_context,
+)
 
 
 class MultitaskStemNet(nn.Module):
@@ -35,14 +44,22 @@ class MultitaskStemNet(nn.Module):
             in_channels=160, out_channels=64, kernel_size=1, stride=1, padding=0
         )
         self.layer5_b = Conv2dBatchActivate(
-            in_channels=64, out_channels=64, kernel_size=(5, 1), stride=1, padding=(2, 0)
+            in_channels=64,
+            out_channels=64,
+            kernel_size=(5, 1),
+            stride=1,
+            padding=(2, 0),
         )
 
         self.layer6_a = Conv2dBatch(
             in_channels=64, out_channels=96, kernel_size=3, stride=1, padding=1
         )
         self.layer6_b = Conv2dBatchActivate(
-            in_channels=64, out_channels=64, kernel_size=(1, 5), stride=1, padding=(0, 2)
+            in_channels=64,
+            out_channels=64,
+            kernel_size=(1, 5),
+            stride=1,
+            padding=(0, 2),
         )
         self.layer7_b = Conv2dBatch(
             in_channels=64, out_channels=96, kernel_size=3, stride=1, padding=1
@@ -55,7 +72,8 @@ class MultitaskStemNet(nn.Module):
         self.layer8_b = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
 
         self.layer9 = SeparableResidualModule(
-            in_channels=384, out_channels=576, kernel_size=3, padding=1)
+            in_channels=384, out_channels=576, kernel_size=3, padding=1
+        )
 
     def forward(self, x):
         x = self.layer0(x)
@@ -87,80 +105,96 @@ class PredictionBlockPS(nn.Module):
      Prediction block is implemented as multi-resolution CNN for Pose Estimation
     """
 
-    def __init__(self, num_context_per_joint=2, Nj=16, Nd=16):
+    def __init__(
+        self,
+        num_context_per_joint=2,
+        Nj=16,
+        Nd=16,
+        num_blocks=8,
+        kernel_size=(5, 5),
+        export_heatmaps=False,
+        concat_pose_confidence=True,
+    ):
         super().__init__()
-        self.layer0_b = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
-        self.layer1_b = ActivateConv2dBatch(
-            in_channels=576, out_channels=288, kernel_size=1)
-        self.layer2_b = SeparableResidualModule(
-            in_channels=288, out_channels=288, kernel_size=5, padding=2)
 
-        self.layer3_c = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
-        self.layer4_c = SeparableResidualModule(
-            in_channels=288, out_channels=288, kernel_size=5, padding=2)
+        self.num_blocks = num_blocks
+        self.kernel_size = kernel_size
+        self.concat_pose_confidence = concat_pose_confidence
+        self.export_heatmaps = export_heatmaps
 
-        self.layer5_a = SeparableResidualModule(
-            in_channels=576, out_channels=576, kernel_size=5, padding=2)
-        self.layer5_b = SeparableResidualModule(
-            in_channels=288, out_channels=288, kernel_size=5, padding=2)
-        self.layer5_c = SeparableResidualModule(
-            in_channels=288, out_channels=288, kernel_size=5, padding=2)
-
-        self.layer6_c = SeparableResidualModule(
-            in_channels=288, out_channels=288, kernel_size=5, padding=2)
-        self.layer7_c = nn.Upsample(scale_factor=2)
-        self.layer8_b = SeparableResidualModule(
-            in_channels=288, out_channels=576, kernel_size=5, padding=2)
-        self.layer9_b = nn.Upsample(scale_factor=2)
         self.layer10 = ActivateSeparableConv2dBatch(
-            in_channels=576, out_channels=576, kernel_size=5, padding=2)
+            in_channels=576, out_channels=576, kernel_size=self.kernel_size, padding=2
+        )
+
+        self.sam_s_model = SoftArgMax2d(
+            in_channels=Nj, out_channels=Nj, kernel_size=(32, 32)
+        )
+        self.jprob_s_model = BuildJointsProbability(kernel_size=(32, 32), padding=0)
+
+        _num_heatmaps = (num_context_per_joint + 1) * Nj
+
+        self.sam_c_model = SoftArgMax2d(
+            in_channels=_num_heatmaps, out_channels=_num_heatmaps, kernel_size=(32, 32)
+        )
+        self.jprob_c_model = BuildJointsProbability(kernel_size=(32, 32), padding=0)
+
+        self.agg_model = BuildContextAggregation(
+            in_features=1, alpha=0.8, num_joints=Nj, num_context=num_context_per_joint
+        )
+
         # heatmap
         self.layer11 = nn.Sequential(
             nn.ReLU(inplace=True),
             nn.Conv2d(
                 in_channels=576,
-                out_channels=Nd*Nj,
+                out_channels=Nd * Nj,
                 kernel_size=1,
                 stride=1,
                 padding=0,
                 bias=False,
-            )
-
+            ),
         )
-        self.sam_s_model = SoftArgMax2d(
-            in_channels=Nj, out_channels=Nj, kernel_size=(32, 32))
-
-        _num_heatmaps = (num_context_per_joint + 1) * Nj
-
-        self.sam_c_model = SoftArgMax2d(
-            in_channels=_num_heatmaps, out_channels=_num_heatmaps, kernel_size=(32, 32))
 
         self.layer12 = ActivateConv2dBatch(
-            in_channels=Nd*Nj, out_channels=576, kernel_size=1)
+            in_channels=Nd * Nj, out_channels=576, kernel_size=1
+        )
 
     def forward(self, x):
-        x_b = self.layer0_b(x)
-        x_b = self.layer1_b(x_b)
-        x_b = self.layer2_b(x_b)
-        x_c = self.layer3_c(x_b)
-        x_c = self.layer4_c(x_c)
-        x_a = self.layer5_a(x)
-        x_b = self.layer5_b(x_b)
-        x_c = self.layer5_c(x_c)
-        x_c = self.layer6_c(x_c)
-        x_c = self.layer7_c(x_c)
-        x_b = torch.add(x_b, x_c)
-        x_b = self.layer8_b(x_b)
-        x_b = self.layer9_b(x_b)
-        x_a = torch.add(x_a, x_b)
-        x1 = self.layer10(x_a)
-        x2 = self.layer11(x1)
-        x3 = self.layer12(x2)
-        x = torch.add(torch.add(x_a, x1), x3)
-        return x
+        outputs = []
+        for block in range(self.num_blocks):
+            x = BuildReceptionBlock(self.kernel_size)(x)
+            ident_map = x
+
+            x = self.layer10(x)
+            h = self.layer11(x)
+
+            pose, visible, hm = pose_regression_2d_context(
+                h,
+                self.num_joints,
+                self.sam_s_model,
+                self.sam_c_model,
+                self.jprob_c_model,
+                self.agg_model,
+                self.jprob_s_model,
+            )
+
+            if self.concat_pose_confidence:
+                outputs.append(torch.cat([pose, visible]))
+            else:
+                outputs.append(pose)
+                outputs.append(visible)
+
+            if self.export_heatmaps:
+                outputs.append(hm)
+
+            if block < self.num_blocks - 1:
+                h = self.layer12(h)
+                x = torch.add(torch.add(ident_map, x), h)
+
+        return torch.tensor(outputs)
 
 
 net = PredictionBlockPS()
 x = torch.ones([1, 576, 32, 32])
 print(net(x).shape)
-#print(conv2d_out_shape(64, 64, nn.Conv2d(96, 96, (2, 2), 2, (0, 0))))
+# print(conv2d_out_shape(64, 64, nn.Conv2d(96, 96, (2, 2), 2, (0, 0))))
